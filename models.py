a94e2a8c180e10ba07e92fbee5860ee3430612e2
lubwamabenja <lubwama73@gmail.com>
2018-10-29 11:45:22 +0300

    validating candidate scores

---
+++ models.py
@@ -0,0 +1,28 @@
+class Scores:
+        candidates_dict = {}
+        """docstring for Scores"""
+        def validate_scores(self,score,cand_id):
+        score = int(input("Enter the score Mark: "))
+        if score >= -2 and value <= 2:
+                print("The value is out of range, try again.")
+        else:
+            for key in scores_dict:
+                if key == cand_id:
+                    Scores.candidates_dict[cand_id] = score
+                    return "score has been added successfully"
+                else:
+                    return"user_id cannot be found"
+
+
+
+        	
+
+
+
+     
+
+    
+
+	
+		
+		
