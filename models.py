b9c39b92fcaab1753440556ad5ceed5996321f87
lubwamabenja <lubwama73@gmail.com>
2018-10-29 12:26:02 +0300

    (ch-score-models-161540094)chore validates users input [Deliveries #161540094]

--- models.py
+++ models.py
@@ -1,18 +1,20 @@
 class Scores:
     scores_dict = {}
     def validate_scores(self,cand_id,score):
-        score = int(input('Enter students marks: '))
-        if score >= -2 and score <= 2:
-            print("The value is out of range, try again.")
-        else:
-            for key in Scores.scores_dict:
-                if key == cand_id:
-                    score.candidates_dict[cand_id] = score
-                    print('score has been added successfully')
-                else:
-                    print('user_id cannot be found')
-
-
+        '''this method validates the user score input '''
+        try:
+            score = int(input("enter score mark: "))
+            if score >= -2 and score <= 2:
+                print("The value is out of range, try again.")
+            else:
+                for key in Scores.scores_dict:
+                    if key == cand_id:
+                        score.candidates_dict[cand_id] = score
+                        print('score has been added successfully')
+                    else:
+                        print('user_id cannot be found')
+        except ValueError:
+            print("score mark should be an integer")
 
         	
 
